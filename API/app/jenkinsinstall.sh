ef00442567170a972f6e98feaf93093eb36600d4
Mendes Sousa <leoprofessionallogytech@outlook.com>
2023-01-13 17:05:24 -0300

    @v1.4.4

--- API/app/jenkinsinstall.sh
+++ API/app/jenkinsinstall.sh
@@ -1,8 +1,15 @@
-#first install openJdk
+# first install the docker and git
+sudo apt update -y && sudo apt install git && 
+sudo apt install apt-transport-https ca-certificates curl software-properties-common -y && 
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - && 
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable" && 
+sudo apt update && apt-cache policy docker-ce && sudo apt install docker-ce -y && clear
+
+#second install openJdk
 sudo apt update && sudo apt install default-jre &&
 sudo apt install default-jdk && clear
 
-#install jenkins
+#now install jenkins
 wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add - &&
 sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list' &&
 sudo apt update && sudo apt install jenkins && clear
@@ -21,6 +28,9 @@ sudo usermod -aG docker jenkins
 
 #get the jenkins password
 sudo cat /var/lib/jenkins/secrets/initialAdminPassword
+
+#if you need work with ssh integrated a private repository on github or other use it
+ssh-keygen -t rsa -b 4096
 
 #plugins
 
